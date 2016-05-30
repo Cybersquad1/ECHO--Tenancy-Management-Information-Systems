2aaa6fd79186a79d4ed74d94fe767873620b6e6a
Royce Stephan Ignacio <ramil.charles.ignacio@gmail.com>
2016-05-30 11:01:23 +0800

    fixes

---
+++ ReportForms/Billing/Receipt.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tenancy_Management_Information_Systems.ReportForms.Billing
+{
+    partial class Receipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.echoDataSet = new Tenancy_Management_Information_Systems.EchoDataSet();
+            this.receiptBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.receiptTableAdapter = new Tenancy_Management_Information_Systems.EchoDataSetTableAdapters.ReceiptTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.echoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "Tenancy_Management_Information_Systems.Reports.OfficialReceipt.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(712, 375);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // echoDataSet
+            // 
+            this.echoDataSet.DataSetName = "EchoDataSet";
+            this.echoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // receiptBindingSource
+            // 
+            this.receiptBindingSource.DataMember = "Receipt";
+            this.receiptBindingSource.DataSource = this.echoDataSet;
+            // 
+            // receiptTableAdapter
+            // 
+            this.receiptTableAdapter.ClearBeforeFill = true;
+            // 
+            // Receipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 375);
+            this.Controls.Add(this.reportViewer);
+            this.Name = "Receipt";
+            this.Text = "Receipt";
+            this.Load += new System.EventHandler(this.Receipt_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.echoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private EchoDataSet echoDataSet;
+        private System.Windows.Forms.BindingSource receiptBindingSource;
+        private EchoDataSetTableAdapters.ReceiptTableAdapter receiptTableAdapter;
+    }
+}
---
+++ ReportForms/Billing/Receipt.cs
@@ -0,0 +1,45 @@
+﻿using Echo.Data.Repository;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tenancy_Management_Information_Systems.ReportForms.Billing
+{
+    public partial class Receipt : Form
+    {
+        public Receipt()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadReport(Guid _paymentHistoryID)
+        {
+            using (var db = new EchoEntities())
+            {
+                receiptBindingSource.DataSource = db.Receipt(_paymentHistoryID);
+
+                ReportParameter[] rParams = new ReportParameter[]
+                {
+                    
+                };
+
+                reportViewer.LocalReport.SetParameters(rParams);
+
+                reportViewer.RefreshReport();
+            }
+        }
+
+        private void Receipt_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer.RefreshReport();
+        }
+    }
+}
